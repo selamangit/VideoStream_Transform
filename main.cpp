01cc8e00add5a082956312682644d582307757bf
selamangit <selamangit@163.com>
2022-09-28 22:01:36 -0700

    merge:upload project

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+extern "C"
+{
+#include "libavformat/avformat.h"
+#include "libavcodec/avcodec.h"
+#include "libavutil/frame.h"
+
+}
+
+using namespace std;
+int main()
+{
+    
+}
